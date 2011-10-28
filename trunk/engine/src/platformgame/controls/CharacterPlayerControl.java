8e6c256f4aad4f4b6132fa1ca7fb25f9d7b7be5e
sunshinegamestudio <sunshinegamestudio@0c01623f-0f24-8cf4-3583-62b0dc82705a>
2011-10-28 19:46:48 +0000

    Collision for CharacterControls.

--- trunk/engine/src/platformgame/controls/CharacterPlayerControl.java
+++ trunk/engine/src/platformgame/controls/CharacterPlayerControl.java
@@ -9,6 +9,7 @@ import com.jme3.bullet.PhysicsTickListener;
 import com.jme3.bullet.collision.PhysicsCollisionEvent;
 import com.jme3.bullet.collision.PhysicsCollisionListener;
 import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.math.Vector3f;
 
 /**
  *
@@ -44,6 +45,15 @@ public class CharacterPlayerControl extends RigidBodyControl implements PhysicsT
         {
             System.out.println("Collison: Player-SimpleEnemy");
             //Place Player1 and SimpleEnemy1 back, so they are not colliding !!!!!!!!!!!!
+            if("simple_enemy_1-ogremesh".equals(event.getNodeA().getName()))    {
+                //event.getNodeA().move(event.getNodeA().getLocalRotation().multLocal(new Vector3f(0,0,1)).multLocal(-tpf));
+                event.getNodeA().move(event.getNodeA().getLocalRotation().multLocal(new Vector3f(0,0,1)).multLocal(-16));
+            }
+            if("simple_enemy_1-ogremesh".equals(event.getNodeB().getName()))    {
+                //event.getNodeA().move(event.getNodeA().getLocalRotation().multLocal(new Vector3f(0,0,1)).multLocal(-tpf));
+                event.getNodeB().move(event.getNodeB().getLocalRotation().multLocal(new Vector3f(0,0,1)).multLocal(-16));
+            }
+            
         }
 
     }
