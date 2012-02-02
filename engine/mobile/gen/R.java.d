85f90056fa353d9c2a68b3de31d003a050a0e4dd
sunshinegamestudio <sunshinegamestudio@0c01623f-0f24-8cf4-3583-62b0dc82705a>
2012-02-02 21:45:53 +0000

    Recreated mobile settings.

---
+++ engine/mobile/gen/R.java.d
@@ -0,0 +1,4 @@
+G:\Development\GameDev\jMP\PlatformGame\engine\mobile\gen\com\sunshinegamestudio\sqeeck_save_the_beavies\R.java \
+ : G:\Development\GameDev\jMP\PlatformGame\engine\mobile\res\layout\main.xml \
+G:\Development\GameDev\jMP\PlatformGame\engine\mobile\res\values\strings.xml \
+G:\Development\GameDev\jMP\PlatformGame\engine\mobile\AndroidManifest.xml \
