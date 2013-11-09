cfc24adbe3ff43ee74320adad68e4dfc938469c9
sunshinegamestudio <sunshinegamestudio@0c01623f-0f24-8cf4-3583-62b0dc82705a>
2013-11-09 23:51:47 +0000

    First steps for implementing BetterCharacterControl.

--- trunk/engine/mobile/gen/R.java.d
+++ trunk/engine/mobile/gen/R.java.d
@@ -1,4 +1,4 @@
-G:\Development\GameDev\jMP\PlatformGame\engine\mobile\gen\com\sunshinegamestudio\sqeeck_save_the_beavies\R.java \
+G:\Development\GameDev\jMP\PlatformGame\engine\mobile\gen\com\sunshinegamestudio\car_game\R.java \
  : G:\Development\GameDev\jMP\PlatformGame\engine\mobile\res\layout\main.xml \
 G:\Development\GameDev\jMP\PlatformGame\engine\mobile\res\values\strings.xml \
-G:\Development\GameDev\jMP\PlatformGame\engine\mobile\AndroidManifest.xml \
+G:\Development\GameDev\jMP\PlatformGame\engine\mobile\bin\AndroidManifest.xml \
